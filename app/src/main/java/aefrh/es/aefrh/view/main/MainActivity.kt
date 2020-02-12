32f83673396f03df51a3a6beb976782bb9ab6d19
Guillermo Marco <guillermomarcoalija@gmail.com>
2020-02-12 12:14:11 +0100

    Added Firebase
    - Crashlitics
    - Database

--- app/src/main/java/aefrh/es/aefrh/view/main/MainActivity.kt
+++ app/src/main/java/aefrh/es/aefrh/view/main/MainActivity.kt
@@ -3,9 +3,6 @@ package aefrh.es.aefrh.view.main
 import aefrh.es.aefrh.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 
 }
