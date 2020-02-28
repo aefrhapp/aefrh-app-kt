9a10a58c12f07d19d940a722c4e25d3dcd48bae2
Guillermo Marco <guillermomarcoalija@gmail.com>
2020-02-28 11:01:10 +0100

    Epocas UI
    - Added SpanningLayout to fit all vertical screen with Epocas
    - Added container gold in UI
    - Redo of cell items

--- app/src/main/java/aefrh/es/aefrh/presentation/epocas/FragmentEpocas.kt
+++ app/src/main/java/aefrh/es/aefrh/presentation/epocas/FragmentEpocas.kt
@@ -2,9 +2,11 @@ package aefrh.es.aefrh.presentation.epocas
 
 import aefrh.es.aefrh.R
 import aefrh.es.aefrh.presentation.base.BaseFragment
+import aefrh.es.aefrh.utils.SpanningLinearLayoutManager
 import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_epocas.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -21,12 +23,12 @@ class FragmentEpocas: BaseFragment() {
         val adapter = EpocasListAdapter()
         rv_epocas.apply {
             this.adapter = adapter
+            layoutManager = SpanningLinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             postponeEnterTransition()
-            viewTreeObserver
-                .addOnPreDrawListener {
-                    startPostponedEnterTransition()
-                    true
-                }
+            viewTreeObserver.addOnPreDrawListener {
+                startPostponedEnterTransition()
+                true
+            }
         }
 
         vModel.epocas.observe(viewLifecycleOwner, Observer {
