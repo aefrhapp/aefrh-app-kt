ed59b28ee8059d061e8b85df1a670ff6a65cf184
Guillermo Marco <guillermomarcoalija@gmail.com>
2020-03-03 16:26:44 +0100

    UI changes
    - Applied all project MaterialComponent
    - Extracted in Styles text view styles
    - Implemented in project viewpager library to details

--- app/src/main/java/aefrh/es/aefrh/presentation/fiestas/FragmentFiestaDetails.kt
+++ app/src/main/java/aefrh/es/aefrh/presentation/fiestas/FragmentFiestaDetails.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
+import com.custom.sliderimage.logic.SliderImage
 import kotlinx.android.synthetic.main.fragment_fiesta_details.*
+import timber.log.Timber
 
 class FragmentFiestaDetails: BaseFragment() {
 
@@ -24,7 +26,29 @@ class FragmentFiestaDetails: BaseFragment() {
             safeArgs = FragmentFiestaDetailsArgs.fromBundle(it)
         }
 
-        tv_historia.text = safeArgs?.fiestaid ?: "Nothing matter"
+        // add images URLs
+        val images = listOf(
+            "https://ac19oljbfr.files-sashido.cloud/af8f6d4a93b7d22a58761ce580135823_IMG_1578509895746.jpg",
+            "https://ac19oljbfr.files-sashido.cloud/fcdfbd602ab0e57d200b0ad374c9b298_IMG_1578509907487.jpg",
+            "https://ac19oljbfr.files-sashido.cloud/630b3ef64847669acc2cecdf31c25339_IMG_1578509915890.jpg")
+
+        // Add image URLs to sliderImage
+        slider_details.setItems(images)
+        slider_details.addTimerToSlide(4000)
+        slider_details.onPageListener(onPageScroll = { position, offSet, offSetPixels ->
+            Timber.e("position $position  offSet: $offSet  pixels $offSetPixels")
+        }, onPageStateChange = { state ->
+            Timber.e("State change $state")
+        }, onPageSelected = { position ->
+            Timber.e("page select $position")
+        })
+
+//        SliderImage.openfullScreen(items = images, position = 0)
+
+        // Add slider component to a container
+//        container_main_images.addView(slider)
+
+//        tv_historia.text = safeArgs?.fiestaid ?: "Nothing matter"
 
     }
 
