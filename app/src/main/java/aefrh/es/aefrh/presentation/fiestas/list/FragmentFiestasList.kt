9a41560594305e0c07b4a60f63ddc33bf49ffd5e
Guillermo Marco <guillermomarcoalija@gmail.com>
2020-03-07 12:55:40 +0100

    Loading

--- app/src/main/java/aefrh/es/aefrh/presentation/fiestas/list/FragmentFiestasList.kt
+++ app/src/main/java/aefrh/es/aefrh/presentation/fiestas/list/FragmentFiestasList.kt
@@ -1,11 +1,14 @@
 package aefrh.es.aefrh.presentation.fiestas.list
 
 import aefrh.es.aefrh.R
+import aefrh.es.aefrh.domain.Status
 import aefrh.es.aefrh.presentation.base.BaseFragment
 import aefrh.es.aefrh.presentation.fiestas.FiestasViewModel
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_epocas.*
 import kotlinx.android.synthetic.main.fragment_fiesta_list.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -26,11 +29,9 @@ class FragmentFiestasList: BaseFragment() {
                     it
                 )
         }
-
         vModel.getFiestas(safeArgs?.fiestaid)
 
-        val adapter =
-            FiestasListAdapter()
+        val adapter = FiestasListAdapter()
         rv_fiestas.apply {
             this.adapter = adapter
             postponeEnterTransition()
@@ -41,8 +42,22 @@ class FragmentFiestasList: BaseFragment() {
         }
 
         vModel.fiestas.observe(viewLifecycleOwner, Observer {
-            val result = it.data
-            if (!result.isNullOrEmpty()) adapter.submitList(result)
+
+            when(it.status) {
+                Status.LOADING -> {
+                    pb_fiestas_list.visibility = View.VISIBLE
+                }
+                Status.ERROR -> {
+                    pb_fiestas_list.visibility = View.GONE
+                    Toast.makeText(context, R.string.error2, Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    pb_fiestas_list.visibility = View.GONE
+                    val result = it.data
+                    if (!result.isNullOrEmpty()) adapter.submitList(result)
+                }
+            }
+
         })
 
     }
